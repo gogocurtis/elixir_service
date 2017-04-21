8472dca7c93b5094cbf3a165001e4b8448ea30f9
Curtis Schofield <curtis.schofield@indiegogo.com>
2017-04-21 11:41:29 -0700

    order of pattern matchin is essential

--- web/controllers/dns_controller.ex
+++ web/controllers/dns_controller.ex
@@ -59,14 +59,15 @@ defmodule ElixirService.DnsController do
   use ElixirService.Web, :controller
 
 
-  def check_mx(conn, %{}) do
-    conn
-    |>  put_status(422)
-    |>  json(%{"errors" => ["invalid params"]})
-  end
 
   def check_mx(conn, %{"email" => email }) do
     conn
     |> json(MailCheck.check(email))
+  end
+  # fall-through case
+  def check_mx(conn, _params) do
+    conn
+    |>  put_status(422)
+    |>  json(%{"errors" => ["invalid params"]})
   end
 end
